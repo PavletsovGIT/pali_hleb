eb1d15f62e98bc115e14eb71384180d857214140
Feodor Pavletsov <pavletsovgames@gmail.com>
2024-11-25 02:05:16 +0700

    added genPalindrom; rm toUpper; has bugs(i:'qwe'/o:'qqwe');

--- code.asm
+++ code.asm
@@ -3,25 +3,27 @@ includelib kernel32.lib   ; подключаем библиотеку kernel32.lib
 extrn GetStdHandle: proc
 extrn ReadFile: proc
 extrn WriteFile: proc
- 
+
 .data
-buffer byte 200 dup (?)    ; буфер для считывания данных
-len = $ - buffer
- 
+buffer byte 200 dup (?)        ; буфер для считывания данных
+len = $ - buffer               ; длина буфера
+result_buf byte 400 dup (?)    ; буфер для палиндрома (длина удвоенная)
+lens dq 0                      ; длина считанной строки
+
 .code
 
 read proc
-  sub rsp, 56   ; 32 (shadow storage) + 8 (5-й параметр ReadFile) + 8 (byttesRead) + 8 (выравнивание)
-  mov r8, rcx   ;Третий параметр ReadFile - размер буфера
-  mov rcx, rax ; Первый параметр ReadFile - дескриптор файла
-  mov rdx, rdi ; Второй параметр ReadFile - адрес буфера
+  sub rsp, 56   ; 32 (shadow storage) + 8 (5-й параметр ReadFile) + 8 (bytesRead) + 8 (выравнивание)
+  mov r8, rcx   ; Третий параметр ReadFile - размер буфера
+  mov rcx, rax  ; Первый параметр ReadFile - дескриптор файла
+  mov rdx, rdi  ; Второй параметр ReadFile - адрес буфера
   lea r9, bytesRead   ; Четвертый параметр ReadFile - количество считанных байтов
   mov qword ptr [rsp + 32], 0      ; Пятый параметр ReadFile - 0
-  call ReadFile       ; вызов функции RaedFile
+  call ReadFile       ; вызов функции ReadFile
   test rax, rax       ; проверяем на ошибку
   mov eax, bytesRead  ; если ошибки нет, в RAX количество считанных байтов
-  jnz exit          ; если в RAX ненулевое значение
-  mov rax, -1       ; помещаем условный код ошибки
+  jnz exit            ; если в RAX ненулевое значение
+  mov rax, -1         ; помещаем условный код ошибки
 exit:
   add rsp, 56
   ret
@@ -31,38 +33,48 @@ read endp
 readFromConsole proc
   sub rsp, 32
   push rcx            ; сохраняем количество символов
-  mov  rcx, -10         ; Аргумент для GetStdHandle - STD_INPUT
-  call GetStdHandle     ; вызываем функцию GetStdHandle
+  mov  rcx, -10       ; Аргумент для GetStdHandle - STD_INPUT
+  call GetStdHandle   ; вызываем функцию GetStdHandle
   pop rcx
   call read
   add rsp, 32
   ret
 readFromConsole endp
 
-toUpper proc
-  push rcx  ; изменяем rcx, поэтому сохраним его в стеке
- 
-while_start: 
-  test rcx, rcx
-  jz while_end
-  dec rcx
-  cmp byte ptr [rsi + rcx], 122
-  ja while_start
-  cmp byte ptr [rsi + rcx], 97
-  jb while_start
-  and byte ptr [rsi + rcx], 011011111b  ; преобразование к верхнему регистру
-  jmp while_start
-while_end:
-  pop rcx
-  ret
-toUpper endp
+genPalindrome PROC
+    ; Устанавливаем направление обработки цепочек
+    CLD                         ; Устанавливаем направление: вперед
+
+    ; Копируем строку в начало результата
+    lea rsi, buffer             ; Адрес исходной строки
+    lea rdi, result_buf         ; Адрес результирующего буфера
+    mov rcx, qword ptr [lens]   ; Количество символов в строке
+    rep movsb                   ; Копируем строку в результат
+
+    ; Переворачиваем строку и добавляем к результату
+    lea rsi, buffer             ; Адрес исходной строки
+    add rsi, qword ptr [lens]   ; Указатель на конец исходной строки
+    dec rsi                     ; Переходим к последнему символу
+    mov rcx, qword ptr [lens]   ; Количество символов в строке
+    std                         ; Устанавливаем направление: назад
+    rep movsb                   ; Копируем строку в обратном порядке
+
+    ; Завершаем результирующую строку
+    cld                         ; Возвращаем направление: вперед
+    lea rdi, result_buf         ; Указатель на результирующий буфер
+    add rdi, qword ptr [lens]   ; Переход к концу первой копии строки
+    add rdi, qword ptr [lens]   ; Переход к концу второй копии строки
+    mov byte ptr [rdi], 0       ; Завершаем строку символом NULL
+
+    ret
+genPalindrome ENDP
 
 write proc
   sub  rsp, 56
   mov rdx, rsi          ; Второй параметр - строка
   mov r8, rcx           ; Третий параметр - длина строки
   mov  rcx, rax         ; Первый параметр WriteFile - в регистр RCX помещаем дескриптор файла - консольного вывода
-  lea  r9, bytesWritten       ; Четвертый параметр WriteFile - адрес для получения записанных байтов
+  lea  r9, bytesWritten ; Четвертый параметр WriteFile - адрес для получения записанных байтов
   mov qword ptr [rsp + 32], 0  ; Пятый параметр WriteFile
   call WriteFile
      
@@ -79,9 +91,9 @@ write endp
 writeToConsole proc
   sub rsp, 32
   push rcx            ; сохраняем количество символов
-  mov rcx, -11         ; Аргумент для GetStdHandle - STD_OUTPUT
-  call GetStdHandle     ; вызываем функцию GetStdHandle
-  pop rcx         ; восстанавливаем RCX - количество символов
+  mov rcx, -11        ; Аргумент для GetStdHandle - STD_OUTPUT
+  call GetStdHandle   ; вызываем функцию GetStdHandle
+  pop rcx             ; восстанавливаем RCX - количество символов
   call write
   add rsp, 32
   ret
@@ -91,13 +103,15 @@ start proc
   lea rdi, buffer       ; буфер для считывания данных
   mov rcx, len          ; размер буфера
   call readFromConsole  ; считываем данные с консоли
-   
-  lea rsi, buffer   ; обрабатываемая строка
-  mov rcx, rax      ; длина строки - из readFromConsole
-  call toUpper      ; переводим строку в верхний регистр
- 
-  call writeToConsole ; выводим преобразованную строку на консоль
- 
+
+  mov qword ptr [lens], rax ; Сохраняем длину строки
+
+  call genPalindrome    ; Формируем палиндром
+
+  lea rsi, result_buf   ; Выводим палиндром
+  mov rcx, rax          ; Длина результирующей строки
+  call writeToConsole
+
   ret
 start endp
 end
